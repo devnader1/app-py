557e9cb8d2208275ce8bcf143c20b9a9481568b6
devnader1 <devnader7@gmail.com>
2025-07-11 15:36:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Please enter your ID to continue.")
+Id = input("ID: ")
+
+if Id == "1":
+    print("Welcome to the program. {'1'}")
+elif Id == "2":
+    print("Welcome to the program. {'2'}")
+elif Id == "3":
+    print("Welcome to the program. {'3'}")
+elif Id == "4":
+    print("Welcome to the program. {'4'}")
+elif Id == "5":
+    print("Welcome to the program. {'5'}")
+elif Id == "6":
+    print("Welcome to the program. {'6'}")
+elif Id == "7":
+    print("Welcome to the program. {'7'}")
+elif Id == "8":
+    print("Welcome to the program. {'8'}")
+else:
+    print("Invalid ID.")
